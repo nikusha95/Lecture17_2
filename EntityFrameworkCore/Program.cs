fac8f5da6e796750036c0557f0ae24ee0e9fa36c
nikusha95 <nikasanaia11@yahoo.com>
2022-11-30 20:32:48 +0400

    add sql commands

--- EntityFrameworkCore/Program.cs
+++ EntityFrameworkCore/Program.cs
@@ -1,3 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using EntityFrameworkCore;
+using EntityFrameworkCore.Models;
+
+ShopDbContext dbContext = new ShopDbContext();
+
+// await dbContext.ProductTypes.AddAsync(new ProductType
+// {
+//     Name = "Drink"
+// });
+
+//await dbContext.SaveChangesAsync();
+
+
+//add status
+
+var status =  dbContext.Statuses.FirstOrDefault(x => x.Id == 1);
+
+status.Name = "InProgress";
+
+dbContext.Statuses.Update(status);
+
+await dbContext.SaveChangesAsync();//needed when  require to update or add
+
+
+
+
