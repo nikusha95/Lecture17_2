808e22c9f8e37835790e49e07fd93be49b0fd5be
nikusha95 <nikasanaia11@yahoo.com>
2022-11-30 20:44:42 +0400

    load complex object

--- EntityFrameworkCore/Program.cs
+++ EntityFrameworkCore/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Threading.Channels;
 using EntityFrameworkCore;
 using EntityFrameworkCore.Models;
+using Microsoft.EntityFrameworkCore;
 
 ShopDbContext dbContext = new ShopDbContext();
 
@@ -15,14 +17,29 @@ ShopDbContext dbContext = new ShopDbContext();
 
 //add status
 
-var status =  dbContext.Statuses.FirstOrDefault(x => x.Id == 1);
+// var status =  dbContext.Statuses.FirstOrDefault(x => x.Id == 1);
+//
+// status.Name = "InProgress";
+//
+// dbContext.Statuses.Update(status);
+//
+// await dbContext.SaveChangesAsync();//needed when  require to update or add
 
-status.Name = "InProgress";
 
-dbContext.Statuses.Update(status);
+// await dbContext.Orders.AddAsync(new Order
+// {
+//     CustomerId = 1,
+//     StatusId = 1
+// });
+//
+// await dbContext.SaveChangesAsync();
 
-await dbContext.SaveChangesAsync();//needed when  require to update or add
 
+var order = await dbContext.Orders
+    .Include(x=>x.Customer)
+    .Include(x=>x.Status)
+    .FirstOrDefaultAsync(x => x.Id == 1);
 
+Console.WriteLine($"order status {order.Status.Name}");
 
 
